97cecef39f8eb21879fc71f8867a33940dd77c95
SharnaJV <134357065+SharnaJV@users.noreply.github.com>
2023-12-19 16:10:17 +1030

    I'm still working on pushing results to db

--- Main.py
+++ Main.py
@@ -1,12 +1,14 @@
 from tkinter import *
 import random
+from MySQL_Authentication import *
+from widget_events import *
 
 # Setting up the random generator for heads and tails results
 def coin_toss():
     return random.choice(['Heads', 'Tails'])
 
 # Setting up of the Two Up game logic without a betting function
-def play_two_up(guess):
+def play_two_up(username, guess):
     coin_01 = coin_toss()
     coin_02 = coin_toss()
     result = {
@@ -19,24 +21,50 @@ def play_two_up(guess):
         result["outcome"] = "WIN!"
     elif guess == "Two Tails" and coin_01 == coin_02 == "Tails":
         result["outcome"] = "WIN!"
-    elif guess == "Odds - Heads, Tails" and coin_01 == "Heads" and coin_02 == "Tails":
-        result["outcome"] = "WIN!"
-    elif guess == "Odds - Tails, Heads" and coin_01 == "Tails" and coin_02 == "Heads":
-        result["outcome"] = "WIN!"
+    elif guess == "Odds HT" and coin_01 != coin_02:
+        if coin_01 == "Heads":
+            result["outcome"] = "WIN!"
+        else:
+            result["outcome"] = "LOSE!"
+    elif guess == "Odds TH" and coin_01 != coin_02:
+        if coin_01 == "Tails":
+            result["outcome"] = "WIN!"
+        else:
+            result["outcome"] = "LOSE!"
     else:
         result["outcome"] = "LOSE!"
         
     return result
 
-def game_screen():
+def game_screen(username=None):
 
     def play_game():
-        # user_guess = prediction.get()
-        result = play_two_up(prediction.get())
+        user_guess = prediction.get()
+        username = uname_entry.get()
+        result = play_two_up(username, user_guess)
         coin_1_label.config(text=result["coin_1"])
         coin_2_label.config(text=result["coin_2"])
         outcome_label_01.config(text=result["outcome"]) 
         outcome_label_02.config(text=result["outcome"])
+
+#Results are pushed to the DB set up in MySQL_Authentication
+        update_results(username, result["outcome"])
+
+#Read the updated results and display in the GUI
+        wins, losses = read_results(username)
+
+        wins_label.config(text=f"Wins: {wins}")
+        losses_label.config(text=f"Losses: {losses}")
+
+    def username_entry(event):
+        if uname_entry.get() == 'Enter your username':
+            uname_entry.delete(0, "end")
+            uname_entry.config(fg='black')
+
+    def no_username_entry(_):
+        if not uname_entry.get():
+            uname_entry.insert(0, 'Enter your username')
+            uname_entry.config(fg='grey')
         
 #Creating the GUI frame        
     two_up_screen = Tk()
@@ -46,6 +74,14 @@ def game_screen():
     two_up_screen.iconbitmap("ANZAC_logo.ico")
     two_up_screen.geometry("800x450")
     two_up_screen.configure(bg="#EFE0B9")
+
+#Setup of username entry field
+    default_text = 'Enter your username'
+    uname_entry = Entry(two_up_screen, bg="#FFFFFF", fg="grey", font=("Arial", 12))
+    uname_entry.insert(0, default_text)
+    uname_entry.bind('<FocusIn>', username_entry)
+    uname_entry.bind('<FocusOut>', no_username_entry)
+    uname_entry.grid(row=0, column=1, padx=10, pady=5)
     
 #User predicts outcome with radio buttons
     prediction = StringVar()
@@ -77,7 +113,15 @@ def game_screen():
 
     flip_button = Button(two_up_screen, text="FLIP!", width=15, height=2, bg="#B7521E", fg="#EFE0B9", command=play_game)
     flip_button.grid(row=4, column=2, pady=10)
+
+#Labels for showing the wins and the losses in the GUI
+    wins_label = Label(two_up_screen, text="", bg="#EFE0B9", fg="#B7521E", font=("Arial", 16))
+    wins_label.grid(row=5, column=1)
+
+    losses_label = Label(two_up_screen, text="", bg="#EFE0B9", fg="#B7521E", font=("Arial", 16))
+    losses_label.grid(row=5, column=2)
     
     two_up_screen.mainloop()
     
 game_screen()
+my_db_connect.close()
---
+++ MySQL_Authentication.py
@@ -0,0 +1,43 @@
+import mysql.connector
+
+#Setting up the connection to MYSQL DB
+my_db_connect = mysql.connector.connect(
+    host="127.0.0.1",
+    user="root",
+    password="Power2thePeopleWho8",
+    database="twoupapp"
+)
+mycursor = my_db_connect.cursor()
+
+#The program will add 1 to either the wins or losses column depending on the outcome of each round
+def update_results(username, outcome):
+    try:
+        if outcome == "WIN!":
+            query = "UPDATE gameresults SET win_results = win_results + 1 WHERE username = %s"
+            # mycursor.execute("UPDATE gameresults SET win_results = win_results + 1 WHERE username = %s", (username,))
+        else:
+            query = "UPDATE gameresults SET lose_results = lose_results + 1 WHERE username = %s"
+            # mycursor.execute("UPDATE gameresults SET lose_results = lose_results + 1 WHERE username = %s", (username,))
+        print("Executing query:", query)  # Add this line for debugging purposes
+        mycursor.execute(query, (username,))
+    except mysql.connector.Error as error:
+        print("Error updating results:", error)
+    finally:
+        my_db_connect.commit()
+
+#The outcome total of the rounds will show in the GUI
+def read_results(username):
+    try:
+        query = "SELECT win_results, lose_results FROM gameresults WHERE username = %s"
+        print("Executing query:", query)  # Add this line for debugging purposes
+        mycursor.execute(query, (username,))
+        result = mycursor.fetchone()
+        if result:
+            wins, losses = result
+            print("Wins:", wins, "Losses:", losses)  # Print retrieved wins and losses
+            return wins, losses
+        else:
+            return 0, 0
+    except mysql.connector.Error as error:
+        print("Error reading results:", error)
+        return 0, 0
